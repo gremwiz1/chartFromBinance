596d05fb36dd0e191d8b6e495f25924b620b3c06
gremwiz1 <gremwiz@yandex.ru>
2024-10-23 16:18:18 +0300

    refactoring app

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import CandlestickChart from './components/candlestickChart';
 
 const App: React.FC = () => {
-    const [interval, setInterval] = useState<string>('1h'); // Состояние для выбора таймфрейма
+    const [timeframe, setTimeframe] = useState<string>('1h'); 
+
+    // Создаем мемоизированные функции для изменения таймфрейма
+    const handleSetTimeframe1h = useCallback(() => setTimeframe('1h'), []);
+    const handleSetTimeframe5m = useCallback(() => setTimeframe('5m'), []);
 
     return (
         <div className="App">
             <h1>Свечной график BTC/USDT</h1>
             <div>
-                {/* Кнопки для изменения интервала отображения графика */}
-                <button onClick={() => setInterval('1h')}>1 час</button>
-                <button onClick={() => setInterval('5m')}>5 минут</button>
+                {/* Используем мемоизированные функции для изменения интервала */}
+                <button onClick={handleSetTimeframe1h}>1 час</button>
+                <button onClick={handleSetTimeframe5m}>5 минут</button>
             </div>
             {/* Компонент графика с передачей выбранной пары и интервала */}
-            <CandlestickChart symbol="BTCUSDT" interval={interval} />
+            <CandlestickChart symbol="BTCUSDT" interval={timeframe} />
         </div>
     );
 };
